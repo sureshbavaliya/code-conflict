a69ef7aced28959f7718171dfa305f50513af041
unknown <suresh.bavaliya565@gmail.com>
2025-07-15 16:46:29 +0530

    rg-deleted

--- main.tf
+++ main.tf
@@ -2,7 +2,3 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "rg0003" {
-  name     = "erg-lerarning1"
-  location = "West Europe"
-}
