031bc76af4c36f1407065f9891cd891c78d3d612
unknown <suresh.bavaliya565@gmail.com>
2025-07-15 16:32:41 +0530

    gr0001 added

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "rg001" {
+  name     = "rg-code-conflict"
+  location = "West Europe"
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
