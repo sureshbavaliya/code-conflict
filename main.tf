d9bd0d4fd5c6d440e087eacf3a856fed5fae25ef
unknown <suresh.bavaliya565@gmail.com>
2025-07-15 16:40:30 +0530

    rg003 added

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg0003" {
+  name     = "erg-lerarning1"
+  location = "West Europe"
+}
