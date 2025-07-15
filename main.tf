604125c7bd0257be8da038046d879c2bdec1eb31
unknown <suresh.bavaliya565@gmail.com>
2025-07-15 16:54:21 +0530

    rg-deel

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "rg001" {
   name     = "rg-code-conflict"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "rg0002" {
-  name     = "rg-learning"
-  location = "West Europe"
-}
